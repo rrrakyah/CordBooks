288d294c8e71e05e207fb44181bd376d51d46269
rrrakyah <rrrakyah@gmail.com>
2021-06-07 17:51:02 +0200

    Updates

--- main.py
+++ main.py
@@ -15,8 +15,12 @@ import markovify
 TOKEN = os.getenv("DISCORD_TOKEN")
 bot = commands.Bot(command_prefix='$')
 
-with open("text") as f:
-    text = f.read()
+config = configparser.ConfigParser()
+config.read("cfg.ini")
+
+BLACKLIST = config.get("lists", "channel_blacklist")
+WHITELIST = config.get("lists", "user_whitelist")
+
 
 
 #Database stuff
@@ -32,31 +36,45 @@ class User(BaseModel):
 
 class Message(BaseModel):
     user = peewee.ForeignKeyField(User, backref='messages')
+    id = peewee.CharField(unique=True)
     content = peewee.TextField()
     created_date = peewee.DateTimeField(default=datetime.datetime.now)
 
 
 @bot.command()
-async def fetch(ctx, arg):
-    if ctx.author == ctx.me:
-        pass
+async def fetch(ctx, member: discord.Member):
+    if ctx.author.id == ctx.me.id:
+        return
     
     #Scraping history
-    messages = await ctx.channel.history(limit=10).flatten()
+    messages = await ctx.channel.history(limit=None).flatten()
     for n in messages:
         if n.content == '' or validators.url(n.content):
-            return
+            continue
         
-        try:
-            user = User.get(User.id == n.author.id)
-        except User.DoesNotExist:
-            user = User.create(id=n.author.id, username=n.author)
+        if '$fetch' in n.content or '$sus' in n.content:
+            continue
         
-        Message.create(user=user, content=n.content, created_date=n.created_at)
+        if n.author.id == member.id:
+            try:
+                user = User.get(User.id == n.author.id)
+            except User.DoesNotExist:
+                user = User.create(id=n.author.id, username=n.author)
+            
+            try:
+                Message.create(user=user, id=n.id, content=n.content, created_date=n.created_at)
+                print(n.author, n.content)
+            except:
+                pass
+        
+    print("Fetch successfull")
     
     
 @bot.command()
 async def sus(ctx, member: discord.Member):
+    if ctx.author.id == ctx.me.id:
+        return
+    
     id = member.id
     
     list = []
@@ -72,7 +90,7 @@ async def sus(ctx, member: discord.Member):
     
     markovText = sep.join(list)
     
-    text_model = markovify.NewlineText(f)
+    text_model = markovify.NewlineText(markovText)
     
     #Handling webhooks
     webhooks = await ctx.channel.webhooks()
@@ -83,12 +101,46 @@ async def sus(ctx, member: discord.Member):
     #Handling black-/whitelists 
     #if ctx.channel.name in BLACKLIST or ctx.author.name not in USER_WHITELIST:
     #    return
-    #Sending message   
-    #await webhook.send(text_model.make_sentence(), username = ctx.author.name, avatar_url = ctx.author.avatar_url)
-
-    await ctx.send(markovText)
-    print(text_model.make_sentence())
+    #Sending message 
     
+    msg = None
+    while msg is None:
+        msg = text_model.make_short_sentence(280)
+    
+    if msg is None:
+        await webhook.send("Failed to generate text, try again", username = member.name, avatar_url = member.avatar_url)
+        return
+    
+    await webhook.send(msg, username = member.name, avatar_url = member.avatar_url)
+    print(msg)
+
+@bot.command()
+async def whitelist(ctx, member: discord.Member):
+    await ctx.send("This command isnt ready yet!")
+    return
+    if ctx.author.id == ctx.me.id:
+        return
+    
+    if member == None:
+        return
+    
+    newList = config.get("lists", "user_whitelist")
+    print(newList)
+    
+    if newList :
+        List = str(member.id)
+    else:
+        List = newList + "," + str(member.id)
+    
+    config["lists"]["user_whitelist"] = List 
+    
+    updateList()
+    
+
+def updateList():
+    with open("cfg.ini", "w") as configfile:
+        config.write(configfile)
+
 
 db.connect()
 db.create_tables([User, Message])
