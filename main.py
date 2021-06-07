02e5c7a16884015c82e9cc142c946ae61f7035ae
rrrakyah <rrrakyah@gmail.com>
2021-06-07 21:50:22 +0200

    Optimized the fetch and sus functions

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import markovify
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 bot = commands.Bot(command_prefix='$')
+client = discord.Client()
 
 config = configparser.ConfigParser()
 config.read("cfg.ini")
@@ -42,32 +43,64 @@ class Message(BaseModel):
 
 
 @bot.command()
-async def fetch(ctx, member: discord.Member):
+async def fetch(ctx, day=7, lim=1500):
+    await ctx.message.delete()
+    
+    if day >=365:
+        day=365
+    
     if ctx.author.id == ctx.me.id:
         return
     
+    if ctx.channel.name in config.get("lists", "channel_blacklist"):
+        await ctx.send("This channel is blacklisted!")
+        return
+    
+    if day != 7:
+        print("Amount of days before: " + str(day))
+    
+    if lim >= 5000:
+        lim=5000
+        
+    print("For amount of messages: " + str(lim))
+    
     #Scraping history
-    messages = await ctx.channel.history(limit=None).flatten()
+    d = datetime.datetime.now() - datetime.timedelta(days=day)
+    
+    messages = await ctx.channel.history(limit=lim, before=d).flatten()
+    await ctx.send("Now fetching " + str(lim) + " messages from " + str(d))
+    
+    i=0
+    a=0
+    b=0
+    
     for n in messages:
         if n.content == '' or validators.url(n.content):
+            b = b+1
             continue
         
-        if '$fetch' in n.content or '$sus' in n.content:
+        if '$fetch' in n.content or '$sus' in n.content or '$hi' in n.content or '$test' in n.content or '$whitelist' in n.content:
+            b = b+1
             continue
         
-        if n.author.id == member.id:
-            try:
-                user = User.get(User.id == n.author.id)
-            except User.DoesNotExist:
-                user = User.create(id=n.author.id, username=n.author)
+        #if n.author.id == member.id:
+        try:
+            user = User.get(User.id == n.author.id)
+        except User.DoesNotExist:
+            user = User.create(id=n.author.id, username=n.author)
             
-            try:
-                Message.create(user=user, id=n.id, content=n.content, created_date=n.created_at)
-                print(n.author, n.content)
-            except:
-                pass
+        try:
+            Message.create(user=user, id=n.id, content=n.content, created_date=n.created_at)
+            a = a+1
+        except:
+            #print("Didnt work:  ", n.author.name, n.content, n.created_at)
+            i = i+1
+            pass
         
-    print("Fetch successfull")
+    print("Fetch successful")
+    print(str(a) + " messages success")
+    print(str(i) + " messages failed")
+    print(str(b) + " messages were commands/links")
     
     
 @bot.command()
@@ -75,6 +108,11 @@ async def sus(ctx, member: discord.Member):
     if ctx.author.id == ctx.me.id:
         return
     
+    members = ctx.guild.members
+    
+    #for member in members:
+    #    print(member.display_name)
+        
     id = member.id
     
     list = []
@@ -85,7 +123,7 @@ async def sus(ctx, member: discord.Member):
         for message in user.messages:
             list.append(message.content)
     except:
-        ctx.send("User has typed any messages yet")
+        await ctx.send("User has database entries yet")
         return
     
     markovText = sep.join(list)
@@ -101,18 +139,25 @@ async def sus(ctx, member: discord.Member):
     #Handling black-/whitelists 
     #if ctx.channel.name in BLACKLIST or ctx.author.name not in USER_WHITELIST:
     #    return
-    #Sending message 
     
+    #Sending message 
+    await ctx.message.delete()
+    
+    i=0
     msg = None
     while msg is None:
         msg = text_model.make_short_sentence(280)
+        i = i+1
+        if i == 500:
+            await webhook.send("Failed to generate text, try again", username = member.name, avatar_url = member.avatar_url)
+            return
     
     if msg is None:
         await webhook.send("Failed to generate text, try again", username = member.name, avatar_url = member.avatar_url)
         return
     
     await webhook.send(msg, username = member.name, avatar_url = member.avatar_url)
-    print(msg)
+
 
 @bot.command()
 async def whitelist(ctx, member: discord.Member):
@@ -144,5 +189,5 @@ def updateList():
 
 db.connect()
 db.create_tables([User, Message])
-bot.run(TOKEN)
+bot.run(TOKEN) & client.run(TOKEN)
 
